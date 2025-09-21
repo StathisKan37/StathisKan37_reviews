a5f4132d6c3b752c727be0861571a9113b06ebae
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-09-21 18:29:03 +0300

    Add files via upload

---
+++ scripts/footer.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+document.getElementById("site-footer").innerHTML = `
+	<h5>&copy;StathisKan's_Reviews ${year}</h5>
+	<h5>All rights reserved</h5>
+`;
---
+++ scripts/header.js
@@ -0,0 +1,6 @@
+document.getElementById("site-header").innerHTML = `
+	<picture>
+		<source media="(min-width: 540px)" srcset="images/StathisKan_reviews_desktop.png">
+		<img src="images/StathisKan_reviews_mobile.png" alt="StathisKan_reviews_logo" width="100%">
+	</picture>
+`;
