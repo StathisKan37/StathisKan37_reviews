9ad14c612631385ea4cdf8e8798232e23ff06c3a
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-09-26 09:30:57 -0400

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,8 @@
-document.getElementById("site-header").innerHTML = `
-	<picture>
-		<source media="(min-width: 540px)" srcset="images/StathisKan_reviews_desktop.png">
-		<img src="images/StathisKan_reviews_mobile.png" alt="StathisKan_reviews_logo" width="100%">
-	</picture>
-`;
+function header_function(page_position) {
+	document.getElementById("site-header").innerHTML = `
+		<picture>
+			<source media="(min-width: 540px)" srcset="`+page_position+`images/StathisKan_reviews_desktop.png">
+			<img src="`+page_position+`images/StathisKan_reviews_mobile.png" alt="StathisKan_reviews_logo" width="100%">
+		</picture>
+	`;
+};
