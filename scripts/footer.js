70dee98f1ea88dcc9e75cb66bff26e90a7ed80bb
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-09-26 09:30:35 -0400

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,15 @@
-const year = new Date().getFullYear();
-document.getElementById("site-footer").innerHTML = `
-	<h5>&copy;StathisKan's_Reviews ${year}</h5>
-	<h5>All rights reserved</h5>
-`;
+function footer_function() {
+	const year = new Date().getFullYear();
+	document.getElementById("site-footer").innerHTML = `
+		<h5>&copy;StathisKan's_Reviews ${year}</h5>
+		<h5>All rights reserved</h5>
+		<br>
+		<div class="container" style="text-align: right;">
+			<a href="#" class="fa fa-facebook"></a>
+			<a href="#" class="fa fa-twitter"></a>
+			<a href="#" class="fa fa-google"></a>
+			<a href="#" class="fa fa-youtube"></a>
+			<a href="#" class="fa fa-reddit"></a>
+		</div>
+	`;
+};
