72051e2700c5fb4896f65fd2a1ec8bc336920cfe
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-10-03 12:50:14 -0400

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,5 @@
 function header_function(page_position) {
 	document.getElementById("site-header").innerHTML = `
-		<picture>
-			<source media="(min-width: 540px)" srcset="`+page_position+`images/StathisKan_reviews_desktop.png">
-			<img src="`+page_position+`images/StathisKan_reviews_mobile.png" alt="StathisKan_reviews_logo" width="100%">
-		</picture>
+		<img src="`+page_position+`images/StathisKan_reviews_mobile.png" alt="StathisKan_reviews_logo" class="site-header">
 	`;
 };
