331d45ede4dd2e01aaeed49c16edc2104ebf632d
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-10-10 13:06:33 -0400

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,5 @@
 function header_function(page_position) {
 	document.getElementById("site-header").innerHTML = `
-		<img src="`+page_position+`images/StathisKan_reviews_mobile.png" alt="StathisKan_reviews_logo" class="site-header">
+		<img src="`+page_position+`images/StathisKan_reviews_logo.png" alt="StathisKan_reviews_logo" class="site-header">
 	`;
 };
